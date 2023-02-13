7808c37a7eecee19cee7c047923ecfb110fc1210
T. Thyer <tthyer@powerx.ai>
2023-02-13 10:18:13 +0100

    Test applying branch vs main rules

--- data/migrations/sql/schemas/metrics/R__100_schemas.sql
+++ data/migrations/sql/schemas/metrics/R__100_schemas.sql
@@ -1,5 +1,5 @@
-CREATE SCHEMA metrics;
+CREATE SCHEMA IF NOT EXISTS metrics;
 
-CREATE TABLE metrics (c1 varchar);
+CREATE TABLE IF NOT EXISTS metrics (c1 varchar);
 
-CREATE TABLE something_else (c1 int);
+CREATE TABLE IF NOT EXISTS something_else (c1 int);
